8b50117e1d9ae79d214f1156ba5cafd9a8e07031
gangwarshivam1408 <shivam.gangwar@ril.com>
2024-05-09 10:14:02 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React, { useState } from 'react'
-import "./App.css"
-import TodoInput from './components/TodoInput'
-import Todolist from './components/TodoList';
+import React, { useState } from "react";
+import "./App.css";
+import TodoInput from "./components/TodoInput";
+import Todolist from "./components/TodoList";
 function App() {
-  const [listTodo,setListTodo]=useState([]);
-  let addList = (inputText)=>{
-    if(inputText!=='')
-      setListTodo([...listTodo,inputText]);
-  }
-  const deleteListItem = (key)=>{
+  const [listTodo, setListTodo] = useState([]);
+  let addList = (inputText) => {
+    if (inputText !== "") setListTodo([...listTodo, inputText]);
+  };
+  const deleteListItem = (key) => {
     let newListTodo = [...listTodo];
-    newListTodo.splice(key,1)
-    setListTodo([...newListTodo])
-  }
+    newListTodo.splice(key, 1);
+    setListTodo([...newListTodo]);
+  };
+  // app commment
   return (
     <div className="main-container">
       <div className="center-container">
-        <TodoInput addList={addList}/>
+        <TodoInput addList={addList} />
         <h1 className="app-heading">TODO</h1>
-        <hr/>
-        {listTodo.map((listItem,i)=>{
+        <hr />
+        {listTodo.map((listItem, i) => {
           return (
-            <Todolist key={i} index={i} item={listItem} deleteItem={deleteListItem}/>
-          )
+            <Todolist
+              key={i}
+              index={i}
+              item={listItem}
+              deleteItem={deleteListItem}
+            />
+          );
         })}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
